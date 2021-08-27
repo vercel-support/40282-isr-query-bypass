fc97ec7c25dcc5398dabf35f5009e99275c8234a
William Li <email@williamli.dev>
2021-08-27 14:54:06 +0800

    imported Next.js ISR Demo project

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -26,6 +26,15 @@ function Home({now}) {
         </p>
 
         <div style={{textAlign: 'left', width: '100%'}}>
+          <a href="https://nextjs-time-api.vercel-support.app/api/time" target="_blank" style={{'textDecoration': 'underline'}}>Time API</a> Returned:
+          <pre>
+            {
+              now
+            }
+          </pre>
+        </div>
+
+        <div style={{textAlign: 'left', width: '100%'}}>
         router.isFallback:
           <pre>
             {
@@ -52,37 +61,6 @@ function Home({now}) {
           </pre>
         </div>
 
-        
-
-        {/* <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div> */}
       </main>
 
       <footer className={styles.footer}>
